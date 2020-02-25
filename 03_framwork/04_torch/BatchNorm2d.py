0e0af99cc7f5dad4225c7b6835b69e741e62b677
songling.gao <songling.gao@ikang.com>
2020-02-25 13:44:57 +0800

    增加batchNorm2d的测试

---
+++ 03_framwork/04_torch/BatchNorm2d.py
@@ -0,0 +1,17 @@
+# encoding:utf-8
+import torch
+import torch.nn as nn
+
+# num_features - num_features from an expected input of size:batch_size*num_features*height*width
+# eps:default:1e-5 (公式中为数值稳定性加到分母上的值)
+# momentum:动量参数，用于running_mean and running_var计算的值，default：0.1
+m = nn.BatchNorm2d(2, affine=True)  # affine参数设为True表示weight和bias将被使用
+# pytorch中的Tensor通道排列顺序是：[batch, channel, height, width]
+input = torch.randn(1, 2, 3, 4)
+output = m(input)
+
+print(input)
+print(m.weight)
+print(m.bias)
+print(output)
+print(output.size())
