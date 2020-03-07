bf014d742d84894d66ee9745f618d67fb2759664
gaosongling <gaosongling@xiaomi.com>
2020-03-07 11:51:54 +0800

    增加*list的参数

--- 01_base/params.py
+++ 01_base/params.py
@@ -1,6 +1,7 @@
 
 def func1(*args):
     print(args)
+    print(type(args))
 
 
 def func2(**args):
@@ -9,3 +10,7 @@ def func2(**args):
 
 func1(1, 2, 3, 4)
 func2(name="hank", age=30)
+
+nums = [12, 23, 34, 56]
+func1(*nums)
+func1(nums)
